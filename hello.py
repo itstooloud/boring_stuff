c750d324d93753d6eafcdf0c67c94e3c44af1129
Chris <chris@Chriss-MacBook-Pro.local>
2017-05-22 14:29:31 -0400

    Adding hello

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+print('what is your fucking name?')
+myName = input()
+print('your name is ' + myName)
+print('The length of your name is ' + str(len(myName)))
+
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge)+ 1) + ' in one year.')
