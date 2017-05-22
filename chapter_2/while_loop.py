84c4cce1ec05a759f9862d75106cfc7517e11f3a
itstooloud <itstooloud@gmail.com>
2017-05-22 17:54:42 -0400

    added whileloop.py

--- chapter_2/while_loop.py
+++ chapter_2/while_loop.py
@@ -1,5 +1,28 @@
-spam = 0
-while spam < 5:
-    print (spam)
-    spam = spam + 1
+##spam = 0
+##while spam < 5:
+##    print (spam)
+##    spam = spam + 1
+##    
+
+##comment out region is an option under the Format menu
+
+#adds up the numbers between 1 and 100
+##counter = 0
+##for num in range(101):
+##    counter = counter + num
+##
+##print (counter)
+
+## importing Python functions
+
+import random
+
+## you can use this:
+
+## from random import *
+## if you do that, you don't have to prefix each 'random' function with the word 'random'
+
+
+for i in range(5):
+    print(random.randint(1,10))
     
